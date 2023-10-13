1ced8ca6e124dfdc0eba5b0e4b0ed300325dd43e
Muhammad Habiby <muhammadhabiby60@gmail.com>
2023-10-13 18:49:18 +0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  email = 'MuhammadHabiby@Stmik.ptk';
+  nim = '212102412';
+  title = 'angular212102412';
+}
